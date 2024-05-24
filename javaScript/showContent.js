1683862406c41f1751ddc970ff92ad170ba43edb
Mirgd <70599203+Mirgd@users.noreply.github.com>
2024-05-25 00:08:18 +0300

    Delete javaScript directory

--- javaScript/showContent.js
+++
@@ -1,8 +0,0 @@
-function showContent(id) {
-    var content = document.getElementById(id + "Content");
-    if (content.style.display === "none") {
-        content.style.display = "block";
-    } else {
-        content.style.display = "none";
-    }
-}
